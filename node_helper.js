562ab6ed834e653e45fffe5ea2fd99f49d2ea1cf
mandridz <evgeny_sidorenko@gmail.com>
2018-02-17 13:43:28 +0300

    first commit

--- node_helper.js
+++ node_helper.js
@@ -5,8 +5,24 @@
  */
 
 const NodeHelper = require('node_helper');
-const socket = require('socket.io').socket;
-const express = require('express').express;
+
+var io = require('socket.io');
+var express = require('express');
+var app = express.createServer()
+var io = io.listen(app);
+
+app.listen(8888);
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+  
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+	});
+});
 
 module.exports = NodeHelper.create({
  	start: function () {
